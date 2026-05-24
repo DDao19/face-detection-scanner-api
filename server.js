94a443167fc5485a5a563870debad3ab6d0acc37
ddao19 <dung.dao19@outlook.com>
2026-05-24 16:56:37 -0500

    fixed port loggin for production

--- server.js
+++ server.js
@@ -102,8 +102,8 @@ app.put('/image', async (req, res) => {
   }
 })
 
+const PORT = process.env.PORT || 3000
 
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("App is running on port 3000...")
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`)
 })
