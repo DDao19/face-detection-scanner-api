04d4c732ff6d867d623e21a90974617c42710e23
ddao19 <dung.dao19@outlook.com>
2026-05-24 20:18:34 -0500

    fixed the rank component which was causing the register issue

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // connect to our database using knex
-// const db = knex({
-//   client: 'pg',
-//   connection: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: false }
-// })
-
 const db = knex({
   client: 'pg',
   connection: {
@@ -52,70 +46,6 @@ app.post('/signin', async (req, res) => {
   }
 })
 
-// app.post('/register', async (req, res) => {
-  
-//   try {
-//     const {firstName, lastName, email, password} = req.body
-//     const hashpw = bcrypt.hashSync(password, 10)
-    
-//     await db.transaction(async (trx) => {  
-//       const saveLoginInfo = await trx('login').insert({
-//         email: email,
-//         hash: hashpw
-//       })
-
-//       const user = await trx('users').insert({
-//         firstname: firstName,
-//         lastname: lastName,
-//         email: email,
-//         joined: new Date()
-//       })
-
-//       const profile = await trx('users').select('*').where('email', email)
-
-//       res.json(profile)
-//     })
-    
-//   } catch (error) {
-//     console.log(error)
-//     res.status(400).json('Error: unable to register')
-//   }
-
-// })
-
-// app.post('/register', async (req, res) => {
-//   try {
-//     const { firstName, lastName, email, password } = req.body
-
-//     const hashpw = bcrypt.hashSync(password, 10)
-
-//     const profile = await db.transaction(async (trx) => {
-
-//       await trx('login').insert({
-//         email,
-//         hash: hashpw
-//       })
-
-//       const [user] = await trx('users')
-//         .insert({
-//           firstname: firstName,
-//           lastname: lastName,
-//           email,
-//           joined: new Date()
-//         })
-//         .returning('*')
-
-//       return user
-//     })
-
-//     res.json(profile)
-
-//   } catch (error) {
-//     console.log("REGISTER ERROR:", error)
-//     res.status(400).json(error.message)
-//   }
-// })
-
 app.post('/register', async (req, res) => {
   try {
     console.log('BODY:', req.body)
@@ -139,7 +69,7 @@ app.post('/register', async (req, res) => {
           joined: new Date()
         })
         .returning('*')
-      console.log("New User: ", user) 
+      
       return user
     })
 
